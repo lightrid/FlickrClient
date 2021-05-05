34a434b80298baeb5148dbbb11de590479056a0e
LightRid <lightrid@gmail.com>
2021-05-05 18:29:41 +0300

    Initial Commit

---
+++ FlickrClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrClient
+//
+//  Created by Mykhailo Kviatkovskyi on 05.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
